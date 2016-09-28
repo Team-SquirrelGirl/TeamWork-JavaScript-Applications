141ace3b5388e06ddd0186404cd0cebcc70fc20e
TonyNikolov <fatalclaw@hotmail.com>
2016-09-28 22:42:55 +0300

    account users class added/not finished

---
+++ models/AccountUser.js
@@ -0,0 +1,17 @@
+import { Validator } from './Validator.js';
+import { User } from './User.js';
+export class AccountUser {
+    signup(userName, password, firstName, lastName, email) {
+        var user = new User(userName, password, firstName, lastName, email);
+
+
+    }
+
+    login(username, password) {
+
+    }
+
+    logout() {
+      
+    }
+}
---
+++ models/User.js
@@ -0,0 +1,54 @@
+export class User {
+    constructor(userName, password, firstName, lastName, email) {
+        this.userNameProperty = userName;
+        this.passwordProperty = password;
+        this.firstNameProperty = firstName;
+        this.lastNameProperty = lastName;
+        this.emailProperty = email;
+        return this;
+    }
+
+    get userNameProperty() {
+        return this.username;
+    }
+
+    set userNameProperty(newName) {
+        this.username = newName;
+
+    }
+
+    get firstNameProperty() {
+        return this.firstName;
+    }
+
+    set firstNameProperty(newName) {
+        validate.name(newName, 'first name');
+        this.firstName = newName;
+    }
+
+    get lastNameProperty() {
+        return this.lastName;
+    }
+
+    set lastNameProperty(newName) {
+        validate.name(newName, 'last name');
+        this.lastName = newName;
+    }
+
+    get passwordProperty() {
+        return this.password;
+    }
+
+    set passwordProperty(newPassword) {
+        validate.password(newPassword, 'password');
+        this.password = newPassword;
+    }
+
+    get emailProperty() {
+        return this.email
+    }
+
+    set emailProperty(newEmail) {
+        this.email = newEmail;
+    }
+}
