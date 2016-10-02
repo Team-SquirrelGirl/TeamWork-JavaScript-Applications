080e2b0777da9a999baa029038ef9b16fa38f70e
Betastate <betastates@gmail.com>
2016-10-02 15:47:56 +0300

    New front-end

---
+++ public/scripts/uievents.js
@@ -0,0 +1,6 @@
+document.getElementById('openLogin').addEventListener('click',function(e){
+  showOnlyThisId('loginForm');
+},false);
+document.getElementById('openRegister').addEventListener('click',function(e){
+  showOnlyThisId('registerForm');
+},false);
